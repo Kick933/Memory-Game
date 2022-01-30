d2e98903edbaf7be3e928cbc2a41e09854233bfe
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-01-30 15:55:30 +0530

    CSS changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Supermercado : ["Supermercado One", "cursive"],
+        Mochiy: ["Mochiy Pop P One", "cursive"]
+      }
+    },
   },
   plugins: [],
 }
