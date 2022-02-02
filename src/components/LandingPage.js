30aec319b090b69652baba1b9d6068e0edf35982
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-02-02 08:35:12 +0530

    Created landing Page.

---
+++ src/components/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function LandingPage({ level, restart,lost}) {
+  if(!lost){
+  return <div className='bg-black text-white w-screen h-screen flex flex-col justify-center items-center'>
+      <p className='font-Supermercado text-4xl font-bold mb-8'>Welcome to Memory Game</p>
+      <p className='mb-8'>Put your memory to test with amazing Pokemons.</p>
+      <button className='bg-green-400 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl' onClick={restart}>Let's Start</button>
+  </div>;
+  }else{
+    return <div className='bg-black text-white w-screen h-screen flex flex-col justify-center items-center'>
+      <p className='font-Supermercado text-4xl font-bold mb-8'>Oops, you lost..</p>
+      <button className='bg-green-400 mb-8 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl' onClick={restart}>Try Again</button>
+      <p className='my-8'>Check out my Portfolio</p>
+      <button className='bg-green-400 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl'>Sure</button>
+  </div>;
+  }
+}
+
+export default LandingPage;
