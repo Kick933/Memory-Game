570ba63be179126b1918014a043db3d12bd56b3d
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-02-02 08:37:52 +0530

    Finished the GamePlay.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
+import {useState, useEffect, useCallback} from 'react'
+import Level from './components/Level';
+import Nav from "./components/Nav";
+import LandingPage from "./components/LandingPage";
 
 
 function App() {
+  const [score, setScore] = useState(0) // Maintains score
+  const [best, setBest] = useState(0) // Stores highScore
+  const [level,setLevel] = useState(0) // Maintains current game Level with maximum level being 8
+  const [lost,setLost] = useState(false)
+  document.title="Memory Game"
+  // Seeds for image array
+  // Reset the game
+  const lostGame = useCallback(() => {
+    setLost(true)
+    setLevel(0)
+  },[])
+  const restart = useCallback(() => {
+    setLost(false)
+    setScore(0)
+    setLevel(1)
+  },[])
+  // Increment Game Level
+  const incrementLvl = useCallback(() => {
+    setLevel(prevLevel => prevLevel + 1)
+  },[])
+  // Increment Score.
+  const incrementScore = useCallback(() => {
+    setScore(prevScore => prevScore + 1)
+  },[])
+  
+  const maxScore = useCallback((arg) => {
+    if(arg === 0 ) return 0 
+    if(arg === 1) return 3
+    return arg + 2 + maxScore(arg - 1)
+  },[])
+    // Get High Score from localStorage and update it if current score is higher
+  useEffect(() => {
+    // gets and sets high score.
+    const highScore = localStorage.getItem('highScore')
+    if(highScore && highScore > best){
+      setBest(highScore)
+    }
+    if(best < score){
+      setBest(score)
+      localStorage.setItem('highScore',JSON.stringify(best))
+    }
+  },[score,best])
+  // Updates level on score reaching.
+  useEffect(() => {
+    if(score === maxScore(level)  && score > 0){
+      incrementLvl()
+    }
+  },[score,level,incrementLvl,maxScore])
   return (
-    <div>Hello</div>
+    <div className="overflow-x-hidden font-Mochiy text-gray-300 select-none text-center">
+    <Nav current={score} best={best}/>
+    {level === 0 || lost ? <LandingPage incrementLvl={incrementLvl} restart={restart} lost ={lost}/> : null}
+    {level > 0 && !lost ? <Level level={level} incrementLvl={incrementLvl} score={score} incrementScore={incrementScore} key={level} lostGame={lostGame} />: null}
+    </div>
   );
 }
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function LandingPage({ level, restart,lost}) {
+function LandingPage({restart,lost}) {
   if(!lost){
   return <div className='bg-black text-white w-screen h-screen flex flex-col justify-center items-center'>
       <p className='font-Supermercado text-4xl font-bold mb-8'>Welcome to Memory Game</p>
@@ -12,7 +12,7 @@ function LandingPage({ level, restart,lost}) {
       <p className='font-Supermercado text-4xl font-bold mb-8'>Oops, you lost..</p>
       <button className='bg-green-400 mb-8 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl' onClick={restart}>Try Again</button>
       <p className='my-8'>Check out my Portfolio</p>
-      <button className='bg-green-400 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl'>Sure</button>
+      <a href="#h" className='bg-green-400 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl'>Sure</a>
   </div>;
   }
 }
