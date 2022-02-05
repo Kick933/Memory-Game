aa972da1c03c8602163be91d20c78d9b4a7b12a0
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-02-05 15:24:24 +0530

    Text correction.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,8 @@ function LandingPage({restart,lost}) {
   if(!lost){
   return <div className='bg-black text-white w-screen h-screen flex flex-col justify-center items-center'>
       <p className='font-Supermercado text-4xl font-bold mb-8'>Welcome to Memory Game</p>
-      <p className='mb-8'>Put your memory to test with amazing Pokemons.</p>
+      <p className='mb-8'>Put your memory to test with amazing pictures.</p>
+      <p className='mb-8'>Just don't click the same twice.</p>
       <button className='bg-green-400 w-32 p-2 hover:bg-green-500 active:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 rounded-xl' onClick={restart}>Let's Start</button>
   </div>;
   }else{
