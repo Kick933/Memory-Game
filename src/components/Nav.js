6b60721f004fe4c6a04a251080543ca8835dabae
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-02-05 15:24:41 +0530

    Changed navbar color for better visibility.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Nav({current,best}) {
-  return <div className='w-screen flex p-4 justify-between items-center bg-indigo-300 dark:bg-gray-500'>
+  return <div className='w-screen flex p-4 justify-between items-center bg-gray-800'>
       <p className='font-Supermercado text-3xl text-indigo-300 font-extrabold'>Memory Game</p>
-      <div className='flex justify-around w-48'>
+      <div className='flex flex-col md:justify-around md:w-48'>
       <p>Score : {current}</p>
       <p>Best : {best}</p>
       </div>
